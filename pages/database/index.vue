0c8d3ecfccb0171072476e181a4c0b2706cedd08
Ransford Larbi <topsquad3552@gmail.com>
2023-08-01 18:04:08 +0000

    setup for the database page

--- pages/database/index.vue
+++ pages/database/index.vue
@@ -1,14 +1,28 @@
 <template>
 	<div class="w-full relative h-full rounded-2xl pb-2">
 		<div id="style-1" :class="[
-			'w-full h-full rounded-2xl bg-neutral-800 col-span-full row-span-full scrollbar text-justify p-2 relative flex flex-col gap-2',
+			'w-full h-full rounded-2xl bg-neutral-800 col-span-full row-span-full scrollbar text-justify p-2 relative flex flex-col',
 			if_search ? 'overflow-hidden' : 'overflow-y-auto'
 		]">
 			<!-- Search -->
 			<Search :visible="if_search" />
 
 			<!-- heading -->
-			<div class="flex w-full justify-between hover:pb-10 transition-all duration-300 ease-in-out">
+			<div ref="header"
+				class="flex w-full justify-between hover:mb-10 transition-all duration-300 ease-in-out relative group">
+
+
+				<div @click="curr_page = ''" :class="[
+					'opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out absolute -bottom-9 left-1/2 -translate-x-1/2 w-56 h-10 cursor-pointer z-[5000] flex justify-center items-end',
+					if_search ? '' : 'tooltip tooltip-secondary tooltip-open tooltip-bottom'
+				]" data-tip="resize panels">
+					<svg data-tip="resize" v-if="is_hover && !if_search" class="w-6 aspect-square"
+						xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+						<path fill="white"
+							d="M10 6a1 1 0 0 0-1 1v4H5.41l.8-.79a1 1 0 0 0-1.42-1.42l-2.5 2.5a1 1 0 0 0-.21.33a1 1 0 0 0 0 .76a1 1 0 0 0 .21.33l2.5 2.5a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42l-.8-.79H9v4a1 1 0 0 0 2 0V7a1 1 0 0 0-1-1Zm11.92 5.62a1 1 0 0 0-.21-.33l-2.5-2.5a1 1 0 0 0-1.42 1.42l.8.79H15V7a1 1 0 0 0-2 0v10a1 1 0 0 0 2 0v-4h3.59l-.8.79a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l2.5-2.5a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76Z" />
+					</svg>
+				</div>
+
 				<div @click="useSearchStore().setIfSearch(true)" v-if="!if_search" ref="search_bar"
 					class="z-[200] w-full flex justify-end rounded-xl search_bar bg-neutral-900 drop-shadow-2xl transition-all duration-300 ease-out outline outline-2 outline-neutral-600">
 					<input ref="search_bar_input" type="text" class="flex-1 bg-transparent px-2"
@@ -21,12 +35,56 @@
 			<!-- heading end -->
 
 			<!-- content -->
-			<div class="w-full h-full rounded-xl flex gap-2">
-				<div class="transition-all duration-500 ease-in-out bg-neutral-900 flex-1 rounded-xl"></div>
-				<div class="transition-all duration-500 ease-in-out bg-neutral-900 w-4/5 rounded-xl"></div>
-				<div class="transition-all duration-500 ease-in-out bg-neutral-900 flex-1 rounded-xl"></div>
+			<div class="w-full h-full rounded-xl flex gap-2 overflow-hidden pt-2">
+
+				<div @click="handlePage('tasks')" :class="[
+					'transition-all duration-500 ease-in-out bg-neutral-900 group rounded-xl relative',
+					curr_page !== 'tasks' ? 'flex-1' : 'w-4/5'
+				]">
+					<span v-if="curr_page != 'tasks' && curr_page != ''"
+						class="vertical-text absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 tracking-[-0.2rem] font-semibold text-neutral-600 group-hover:text-secondary text-[2rem] cursor-pointer transition-all duration-300 ease-out group-hover:scale-110">TASKS</span>
+
+
+					<div v-if="curr_page == 'tasks' || curr_page == ''"
+						class="w-full h-full rounded-xl bg-opacity-10 p-1 overflow-y-auto bg-white" id="style-1">
+
+					</div>
+				</div>
+
+				<div @click="handlePage('requests')" :class="[
+					'transition-all duration-500 ease-in-out bg-neutral-900 group rounded-xl relative',
+					curr_page !== 'requests' ? 'flex-1' : 'w-4/5'
+				]">
+					<span v-if="curr_page != 'requests' && curr_page != ''"
+						class="vertical-text absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 tracking-[-0.2rem] font-semibold text-neutral-600 group-hover:text-secondary text-[2rem] cursor-pointer transition-all duration-300 ease-out group-hover:scale-110">REQUESTS</span>
+
+
+					<div v-if="curr_page == 'requests' || curr_page == ''"
+						class="w-full h-full rounded-xl bg-opacity-10 p-1 overflow-y-auto bg-white" id="style-1">
+
+					</div>
+				</div>
+
+				<div @click="handlePage('announcements')" :class="[
+					'transition-all duration-500 ease-in-out bg-neutral-900 group rounded-xl relative',
+					curr_page !== 'announcements' ? 'flex-1' : 'w-4/5'
+				]">
+					<span v-if="curr_page != 'announcements' && curr_page != ''"
+						class="vertical-text absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 tracking-[-0.2rem] font-semibold text-neutral-600 group-hover:text-secondary text-[2rem] cursor-pointer transition-all duration-300 ease-out group-hover:scale-110">ANNOUNCEMENTS</span>
+
+
+					<div v-if="curr_page == 'announcements' || curr_page == ''"
+						class="w-full h-full rounded-xl bg-opacity-10 p-1 overflow-y-auto bg-white" id="style-1">
+
+					</div>
+				</div>
+
 			</div>
 			<!-- content end-->
+
+			<!-- footer -->
+			<div class="w-full"></div>
+			<!-- footer end -->
 		</div>
 	</div>
 </template>
@@ -41,6 +99,16 @@ useResizeObserver
 const { if_search } = storeToRefs(useSearchStore())
 const search_bar = ref<HTMLDivElement>()
 const search_bar_input = ref<HTMLInputElement>()
+const header = ref()
+const is_hover = useElementHover(header)
+
+const curr_page = ref('')
+
+function handlePage(page: string): void {
+	curr_page.value = page
+	console.log(curr_page.value);
+
+}
 
 onBeforeUnmount(() => {
 	useSearchStore().setIfSearch(false)
