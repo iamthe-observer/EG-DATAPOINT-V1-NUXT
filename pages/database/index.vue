07df5d2b531868d4dba2867b0c6d6f69ce2b3d4d
Ransford Larbi <topsquad3552@gmail.com>
2023-08-02 10:40:51 +0000

    test

--- pages/database/index.vue
+++ pages/database/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="w-full relative h-full rounded-2xl pb-2">
 		<div id="style-1" :class="[
-			'w-full h-full rounded-2xl bg-neutral-800 col-span-full row-span-full scrollbar text-justify p-2 relative flex flex-col',
+			'bg-white max-w-full w-full h-full max-h-full rounded-2xl bg-neutral-80 col-span-full row-span-full scrollbar text-justify p-2 relative flex flex-col',
 			if_search ? 'overflow-hidden' : 'overflow-y-auto'
 		]">
 			<!-- Search -->
