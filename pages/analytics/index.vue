2a7f9ae32dc9fc96b892c26282d597a01f56cd04
Ransford Larbi <topsquad3552@gmail.com>
2023-08-06 08:59:25 +0000

    init analytics page

--- pages/analytics/index.vue
+++ pages/analytics/index.vue
@@ -1,9 +1,11 @@
 <template>
 	<div class="w-full h-full flex flex-col rounded-2xl pb-2">
 		<div
-			class="w-full h-full rounded-2xl bg-neutral-800 col-span-full row-span-full pb-2 overflow-y-scroll text-justify px-0 relative"
+			class="w-full h-full rounded-2xl bg-neutral-800 dark:bg-neutral-50 dark:shadow-xl col-span-full row-span-full pb-2 overflow-y-auto text-justify px-0 relative grid place-items-center"
 			id="style-1">
 
+
+			<span class="text-4xl">Coming Soon</span>
 
 
 		</div>
